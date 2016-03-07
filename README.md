0bd9f8dcc9d9ad13685ad64fd084824e79f09c81
Victor Nikitash <nikitashvictor@pdffiller.com>
2016-03-07 14:01:19 +0200

    NVV | NGinx with redis sh installaion

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nginx_redis_sh
 sh script for nginx + redis module
+
+#Requirements
+1. Run sh from with sudo rights
+2. clone this repo in opt folder or change "cp -i /opt/nginx.conf /opt/nginx/config/nginx.conf" to "cp -i /path/to/cloned/project/nginx.conf /opt/nginx/config/nginx.conf"
+
+#Links
+nginx + redis [module](https://github.com/openresty/redis2-nginx-module).
+Redis installation [there](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-redis).
---
+++ nginx_redis.sh
@@ -0,0 +1,26 @@
+cd /opt
+apt-get update
+apt-get install libpcre3 libpcre3-dev  zlib1g-dev git build-essential tcl8.5
+wget 'http://nginx.org/download/nginx-1.9.12.tar.gz'
+tar -xzvf nginx-1.9.12.tar.gz
+rm -rf nginx-1.9.12.tar.gz
+cd nginx-1.9.12/
+./configure --prefix=/opt/nginx --add-module=/opt/redis2-nginx-module
+make -j2
+make install
+cp -i /opt/nginx.conf /opt/nginx/config/nginx.conf
+cd ..
+wget -O init-deb.sh https://www.linode.com/docs/assets/660-init-deb.sh
+mv init-deb.sh /etc/init.d/nginx
+chmod +x /etc/init.d/nginx
+/usr/sbin/update-rc.d -f nginx defaults
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+rm -rf redis-stable.tar.gz
+cd redis-stable
+make
+make install
+cd utils
+./install_server.sh
+service redis_6379 start
+service nginx restart
